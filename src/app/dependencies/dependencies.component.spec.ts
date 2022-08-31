2a863046493f4b31446d4f5bd90e8688d410b3f8
Toni Hervas <ahervas@topcon.com>
2022-08-31 18:51:15 +0200

    Changed use of testbed.get for the testbed.inject, as of Angular 12 get deprecation

--- src/app/dependencies/dependencies.component.spec.ts
+++ src/app/dependencies/dependencies.component.spec.ts
@@ -1,36 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MockProvider, MockPipe } from 'ng-mocks';
-import * as td from 'testdouble';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { MockProvider, MockPipe } from "ng-mocks";
+import * as td from "testdouble";
 
-import { DependenciesComponent } from './dependencies.component';
-import { DummyService } from './dummy.service';
-import { AnotherDummyService } from './anotherDummy.service';
-import { DummyPipe } from './dummyPipe.pipe';
+import { DependenciesComponent } from "./dependencies.component";
+import { DummyService } from "./dummy.service";
+import { AnotherDummyService } from "./anotherDummy.service";
+import { DummyPipe } from "./dummyPipe.pipe";
 
-describe('DependenciesComponent', () => {
+describe("DependenciesComponent", () => {
   let component: DependenciesComponent = null;
   let fixture: ComponentFixture<DependenciesComponent> = null;
   let anotherDummyServiceMock: AnotherDummyService = null;
-  let lovelyText: string = 'lovely!';
+  let lovelyText: string = "lovely!";
 
   beforeEach(() => {
     //usamos testdouble para crear un mock y poder modificar el comportamiento de un servicio
     anotherDummyServiceMock = td.constructor(AnotherDummyService).prototype;
-    td.when(anotherDummyServiceMock.lovelyMethod()).thenReturn({ text: lovelyText });
+    td.when(anotherDummyServiceMock.lovelyMethod()).thenReturn({
+      text: lovelyText,
+    });
 
     TestBed.configureTestingModule({
       declarations: [
         DependenciesComponent,
         //ng-mocks tiene un metodo mockpipe donde se mockea facilmente una pipe
-        MockPipe(DummyPipe)
+        MockPipe(DummyPipe),
       ],
       providers: [
         //ng-mocks tiene un metodo mockpipe donde se mockea facilmente una servicio
-        //e incluse se pueden customizar 
-        MockProvider(DummyService, { awesomeMethod: () =>  'awesome!' }),
+        //e incluse se pueden customizar
+        MockProvider(DummyService, { awesomeMethod: () => "awesome!" }),
         //es este caso estamos usando un mock de testdouble, para poder tener acceso facil a metodos
-        { provide: AnotherDummyService, useValue: anotherDummyServiceMock }
-      ]
+        { provide: AnotherDummyService, useValue: anotherDummyServiceMock },
+      ],
     });
 
     fixture = TestBed.createComponent(DependenciesComponent);
@@ -38,28 +40,31 @@ describe('DependenciesComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 
   it('should show "lovely" as a subtitle', () => {
     const hostElement: HTMLElement = fixture.nativeElement;
-    const subtitleDisplay: HTMLElement = hostElement.querySelector('p');
+    const subtitleDisplay: HTMLElement = hostElement.querySelector("p");
 
     expect(subtitleDisplay.textContent).toEqual(lovelyText);
   });
 
-  it('should use TestBed.get to get the dummyservice instance', () => {
+  it('should show "awesome" when component calling to oninit hook', () => {
     fixture.detectChanges();
-    const titleDisplay: HTMLElement = fixture.nativeElement.querySelector('h1');
+    const titleDisplay: HTMLElement = fixture.nativeElement.querySelector("h1");
 
-    expect(titleDisplay.textContent).toEqual('awesome!');
+    expect(titleDisplay.textContent).toEqual("awesome!");
   });
 
-  it('should use TestBed.get to get the dummyservice instance', () => {
-    //mediante el Testbed.get() podemos obtener la instancia de servicios, pipes...
-    const anotherDummyServiceInstance: AnotherDummyService = TestBed.get(AnotherDummyService);
+  it("should use TestBed.inject to get the dummyservice instance", () => {
+    //mediante el Testbed.inject() podemos obtener la instancia de servicios, pipes...
+    const anotherDummyServiceInstance: AnotherDummyService =
+      TestBed.inject(AnotherDummyService);
 
-    expect(anotherDummyServiceInstance.lovelyMethod()).toEqual({ text: lovelyText });
+    expect(anotherDummyServiceInstance.lovelyMethod()).toEqual({
+      text: lovelyText,
+    });
   });
 });
